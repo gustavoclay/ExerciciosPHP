b0d36a5ebed75ccc165c0c95761941afef8635d1
Gustavo Clay <gustavoclay@gmail.com>
2017-09-15 22:20:28 -0300

    Exercicio 15_09_2017

---
+++ basic.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" lang="pt-BR">
+	<title></title>
+</head>
+<body>
+	<?php
+			//Primeiros Passos
+		echo "Hello Word echo /PT-BR ÇÃO"; //comentário de uma linha
+		/*comentário
+		mais de uma linha*/
+		echo "<br>";
+		print "Hello Word print";
+		echo "<br>";
+		echo "<br>";
+
+		// Titulo
+		echo " <h1> Titulo h1 </h1> ";
+
+		// Declação de Variaveis
+		$data = date("d/m/y");
+
+		// Contacatenação
+		print "Hoje é " . $data . " entendeu";
+		echo "<br>";
+		echo "<br>";
+
+		//Calculo com variáveis simples
+		$valor1 = 10;
+		$valor2 = 50;
+		$total = $valor1 + $valor2;
+		$total1 = $valor1 - $valor2;
+		$total2 = $valor1 * $valor2;
+		$total3 = $valor1 / $valor2;
+		$total4 = $valor1 % $valor2;
+
+		echo $total."<br>";
+		echo $total1."<br>";
+		echo $total2."<br>";
+		echo $total3."<br>";
+		echo $total4."<br>";
+
+		//Variavéis de sistemas
+		//$_REQUEST[""] 
+		//$_GET[""] manda a informação pela url
+		//$_POST[""] oculta informação da url
+		
+		// Passando parametros pela URL
+		// @ = isset
+		//if ($_REQUEST["a"] > $_REQUEST["b"]) {
+		//	echo $_REQUEST["a"] . " é maior que " . $_REQUEST["b"];
+		//} elseif ($_REQUEST["b"] > $_REQUEST["a"]) {
+		//	echo $_REQUEST["b"] . " é maior que " . $_REQUEST["a"];
+
+
+
+		//Estruturas de controle
+		$a = 1;
+		$b = 3;
+		if ($a > $b) {
+			echo $a . " é maior que " . $b;
+		} elseif ($b > $a) {
+			echo $b . " é maior que " . $a;
+		} else {
+			echo $a . " é igual a " . $b;
+		}
+		echo "<br>";
+
+		?>
+
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" lang="pt-BR">
+	<title></title>
+</head>
+<body>
+	<h1>Cauculadora</h1>
+	<form action="index.php" method="get">
+		Soma
+		A: <input type="number" name="a">
+		B: <input type="number" name="b">
+		<br>
+		<br>
+		Operação:
+		+<input type="checkbox" name="soma">
+		-<input type="checkbox" name="sub">
+		*<input type="checkbox" name="mult">
+		/<input type="checkbox" name="div">
+		<br>
+		<br>
+		<button type="submit">Calcular</button>
+	</form>
+
+	<?php
+
+	if (@$_REQUEST["soma"]) {
+		$soma =  @$_REQUEST["a"] + $_REQUEST["b"];
+		echo "Resultado: " . $soma;
+	} elseif (@$_REQUEST["sub"]) {
+		$sub =  @$_REQUEST["a"] - @$_REQUEST["b"];
+		echo "Resultado: " . $sub;
+	} elseif (@$_REQUEST["mult"]) {
+		$mult =  @$_REQUEST["a"] * @$_REQUEST["b"];
+		echo "Resultado: " . $mult;
+	} elseif (@$_REQUEST["div"]) {
+		$div =  @$_REQUEST["a"] / @$_REQUEST["b"];
+		echo "Resultado: " . $div;
+	}
+	?>
+
+</body>
+</html>
