3f60bafbb52b2978d5e340e2c929b98fef8cc9f7
Gustavo Clay <gustavoclay@gmail.com>
2017-09-26 18:30:35 -0300

    Iniciando curso em video PHP

---
+++ PHPCursoEmVideo/types.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="_css/estilo.css">
+	<title>Types</title>
+</head>
+<body>
+	<div>
+		<?php
+		$n = 4;
+		$nome = "Gustavo";
+		echo $nome;
+
+
+
+		?>
+	</div>
+</body>
+</html>
