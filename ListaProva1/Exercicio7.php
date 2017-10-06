6a2043c42175dabbcc0c66cca63acd885b99151c
Gustavo Clay <gustavoclay@gmail.com>
2017-10-06 18:25:18 -0300

    Lista para prova terminada

--- ListaProva1/Exercicio7.php
+++ ListaProva1/Exercicio7.php
@@ -1,31 +1,45 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Exercicio 6</title>
+	<title>Exercicio 7</title>
 	<meta charset="utf-8" lang="pt-BR">
-<!--7. Escreva uma função que recebe, por parâmetro, o número de linhas, colunas e um conteúdo (texto) qualquer. Que serão utilizados para criar uma tabela HTML com essas características. -->
+	<link rel="stylesheet" type="text/css" href="Exercicio7.css">
+	<!--7. Escreva uma função que recebe, por parâmetro, o número de linhas, colunas e um conteúdo (texto) qualquer. Que serão utilizados para criar uma tabela HTML com essas características. -->
 </head>
 <body>
-	<h1>Criador de Tableas</h1>
+	<h1>Criador de Tabelas</h1>
 	<p></p>
 	<br>
-	<form action="Exercicio6.php" method="GET">
+	<form action="Exercicio7.php" method="GET">
 		Linhas: <input type="number" name="linhas"><br>
 		Colunas: <input type="number" name="colunas"><br>
-		Conteudo: <input type="number" name="conteudo"><br>
+		Conteudo: <input type="string" name="conteudo"><br>
 		<button type="submit">Criar</button>
 	</form>
 	<br>
-	<h2>Resultado</h2>
+	<h2>Tabela</h2>
 
 	<?php
+	$linhas = @$_REQUEST["linhas"];
+	$colunas = @$_REQUEST["colunas"];
+	$conteudo = @$_REQUEST["conteudo"];
 
-	
+	function criaTabela($linhas, $colunas, $conteudo){
+		echo "<table>\n";
+		for ($i=0; $i < $linhas; $i++) {
+			echo "<tr>\n";
+			for ($j=0; $j < $colunas; $j++) {
+				echo "<td>$conteudo</td>\n";
+			}
+			echo "</tr>\n";
+		}
 
+		echo "</table>\n";
+	};
 
+	echo criaTabela($linhas, $colunas, $conteudo);
 
-
-	 ?>
+	?>
 
 
 </body>
---
+++ ListaProva1/Exercicio8.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Exercicio 8</title>
+	<meta charset="utf-8" lang="pt-BR">
+	<link rel="stylesheet" type="text/css" href="Exercicio7.css">
+	<!--Criar uma função para calcular o imposto de renda dos contribuintes, considerando que os dados de cada contribuinte, número de CPF, número de dependentes e renda mensal são valores fornecidos pelo usuário. Para cada contribuinte será feito um desconto de 5% do salário mínimo por dependente. 
+
+Os valores para cálculo do imposto são: 
+
+Renda Líquida
+Alíquota
+Até 3 salários mínimos
+Isento
+3 salários mínimos
+7%
+4 salários mínimos
+15%
+5 salários mínimos
+22%
+Acima de 6 salários mínimos
+27% -->
+</head>
+<body>
+	<h1>Calcular imposto de Renda</h1>
+	<p>Preencha o formulário para calcular o imposto de renda dos contribuintes</p>
+	<br>
+	<form action="Exercicio8.php" method="GET">
+		Nome: <input type="string" name="nome"><br>
+		CPF: <input type="string" name="cpf"><br>
+		Numero de dependentes: <input type="number" name="qtdDependentes"><br>
+		Renda Mensal: <input type="number" step=0.01 name="renda"><br>
+		<button type="submit">Calcular Imposto de Renda</button>
+	</form>
+	<br>
+	<h2>Resultado</h2>
+
+	<?php
+	$nome = @$_REQUEST["nome"];
+	$cpf = @$_REQUEST["cpf"];
+	$qtdDependentes = @$_REQUEST["qtdDependentes"];
+	$renda = @$_REQUEST["renda"];
+
+	function calculaImpostoRenda($qtdDependentes, $renda){
+		$salarioMinimo = 937;
+		//dependentes
+		if ($qtdDependentes > 0) {
+			$salarioMinimo = (937/100) * (100-($qtdDependentes*5));
+		}
+		//imposto
+		if ($renda <= ($salarioMinimo * 3)) {
+			$imposto = 0;
+		} elseif (($renda > ($salarioMinimo * 3)) and $renda <= (($salarioMinimo * 4))) {
+			$imposto = 7;
+		} elseif (($renda > ($salarioMinimo * 4)) and $renda <= (($salarioMinimo * 5))) {
+			$imposto = 15;
+		} elseif (($renda > ($salarioMinimo * 5)) and $renda <= (($salarioMinimo * 6))) {
+			$imposto = 22;
+		} elseif ($renda > ($salarioMinimo * 6)) {
+			$imposto = 27;
+		}
+		//valor imposto
+		$valorImposto = ($renda/100)*$imposto;
+		return $valorImposto;
+	}
+
+	echo "Valor do imposto: ".calculaImpostoRenda($qtdDependentes, $renda);
+
+	?>
+
+
+</body>
+</html>
