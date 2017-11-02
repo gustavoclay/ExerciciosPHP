d7d91198aab10612c6d7961cbd65479969f4fe63
Gustavo Clay <gustavoclay@gmail.com>
2017-11-02 20:36:44 -0200

    Estudando operadores logicos

--- PHPCursoEmVideo/operadores_logicos.php
+++ PHPCursoEmVideo/operadores_logicos.php
@@ -8,7 +8,8 @@
 <body>
 	<div>
 		<?php
-		$dtNasc = $_GET["dtNasc"];
+		$i = $_GET["i"];
+		$anNasc = $_GET["anNasc"];
 		/*
 		Operador3s logicos
 		and &&      operador E
@@ -18,12 +19,16 @@
 
 		*/
 
-		if ($dtNasc>18 or $dtNasc<65) {
+		if ($i>18 or $i<65) {
 			echo "Participação em votação obrigatória";
 		} else {
 			echo "Participação em votação não obrigatória";
-		}
+		};
 
+		$idade = 2017 - $anNasc;
+		echo "<br>Quem nasceu em $anNasc tem idade de $idade anos.";
+		$tipo = $idade>18 or $idade <65? $tipo = "obrigatório" : $tipo = "não obrigatório";
+		echo " E dessa forma seu voto é $tipo";
 
 		?>
 	</div>
